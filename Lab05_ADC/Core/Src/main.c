45def815881b810306b83f56f0e43c856a008cfe
Phumphathai Chansriwong <ouoam2555@gmail.com>
2020-10-09 10:15:03 +0700

    Add Lab 5 and rename Lab 4 filename

---
+++ Lab05_ADC/Core/Src/main.c
@@ -0,0 +1,265 @@
+/* USER CODE BEGIN Header */
+/**
+  ******************************************************************************
+  * @file           : main.c
+  * @brief          : Main program body
+  ******************************************************************************
+  * @attention
+  *
+  * <h2><center>&copy; Copyright (c) 2020 STMicroelectronics.
+  * All rights reserved.</center></h2>
+  *
+  * This software component is licensed by ST under BSD 3-Clause license,
+  * the "License"; You may not use this file except in compliance with the
+  * License. You may obtain a copy of the License at:
+  *                        opensource.org/licenses/BSD-3-Clause
+  *
+  ******************************************************************************
+  */
+/* USER CODE END Header */
+/* Includes ------------------------------------------------------------------*/
+#include "main.h"
+#include "adc.h"
+#include "dma.h"
+#include "usart.h"
+#include "gpio.h"
+
+/* Private includes ----------------------------------------------------------*/
+/* USER CODE BEGIN Includes */
+#include <stdio.h>
+/* USER CODE END Includes */
+
+/* Private typedef -----------------------------------------------------------*/
+/* USER CODE BEGIN PTD */
+
+/* USER CODE END PTD */
+
+/* Private define ------------------------------------------------------------*/
+/* USER CODE BEGIN PD */
+
+/* --------------------- Change question to compile here ---------------------*/
+/* ---------------------- Available question 1, 3, 4 and 5 ----------------------*/
+#define QUESTION 5
+
+/* USER CODE END PD */
+
+/* Private macro -------------------------------------------------------------*/
+/* USER CODE BEGIN PM */
+
+/* USER CODE END PM */
+
+/* Private variables ---------------------------------------------------------*/
+
+/* USER CODE BEGIN PV */
+
+/* USER CODE END PV */
+
+/* Private function prototypes -----------------------------------------------*/
+void SystemClock_Config(void);
+/* USER CODE BEGIN PFP */
+void displayHEX(uint32_t num) {
+	char tmp[15];
+	sprintf(tmp, "0x%08X", num);
+	HAL_UART_Transmit(&huart3, (unsigned char*) tmp, 10, 100);
+}
+/* USER CODE END PFP */
+
+/* Private user code ---------------------------------------------------------*/
+/* USER CODE BEGIN 0 */
+
+void HAL_ADC_ConvCpltCallback(ADC_HandleTypeDef* hadc)
+{
+  HAL_GPIO_WritePin(LD2_GPIO_Port, LD2_Pin, GPIO_PIN_RESET);
+}
+
+void HAL_ADC_ConvHalfCpltCallback(ADC_HandleTypeDef* hadc)
+{
+  HAL_GPIO_WritePin(LD2_GPIO_Port, LD2_Pin, GPIO_PIN_SET);
+}
+
+/* USER CODE END 0 */
+
+/**
+  * @brief  The application entry point.
+  * @retval int
+  */
+int main(void)
+{
+  /* USER CODE BEGIN 1 */
+
+  /* USER CODE END 1 */
+
+  /* MCU Configuration--------------------------------------------------------*/
+
+  /* Reset of all peripherals, Initializes the Flash interface and the Systick. */
+  HAL_Init();
+
+  /* USER CODE BEGIN Init */
+
+  /* USER CODE END Init */
+
+  /* Configure the system clock */
+  SystemClock_Config();
+
+  /* USER CODE BEGIN SysInit */
+
+  /* USER CODE END SysInit */
+
+  /* Initialize all configured peripherals */
+  MX_GPIO_Init();
+  MX_DMA_Init();
+  MX_ADC1_Init();
+  MX_USART3_UART_Init();
+  /* USER CODE BEGIN 2 */
+HAL_UART_Transmit(&huart3, (unsigned char*)"START\r\n\r\n", 9, 100);
+  /* USER CODE END 2 */
+
+  /* Infinite loop */
+  /* USER CODE BEGIN WHILE */
+#if QUESTION == 1 || QUESTION == 3 || QUESTION == 4
+  volatile uint32_t adc_val = 0;
+  HAL_ADC_Start(&hadc1);
+#elif QUESTION == 5
+  volatile uint32_t adc_val[100];
+  HAL_ADC_Start_DMA(&hadc1, (uint32_t *)adc_val, 100);
+#endif
+  while (1)
+  {
+#if QUESTION == 1
+	  uint32_t hex1 = 501;
+	  displayHEX(hex1);
+	  HAL_Delay(500);
+#elif QUESTION == 3 || QUESTION == 4
+	  while (HAL_ADC_PollForConversion(&hadc1, 100) != HAL_OK);
+	  adc_val = HAL_ADC_GetValue(&hadc1);
+
+	  HAL_UART_Transmit(&huart3, (unsigned char*)"ADC1_CH10 ", 10, 100);
+	  displayHEX(adc_val);
+	  HAL_UART_Transmit(&huart3, (unsigned char*)" Vin = ", 7, 100);
+	  char tmp[10];
+	  int a = sprintf(tmp, "%.2f", adc_val * 3.33 / 0x1000);
+	  HAL_UART_Transmit(&huart3, (unsigned char*)tmp, a, 100);
+	  HAL_UART_Transmit(&huart3, (unsigned char*)"V\r\n", 3, 100);
+
+	#if QUESTION == 4
+	  switch(adc_val / ( 0x1000 / 5)) {
+	  case 0: HAL_GPIO_WritePin(GPIOC, GPIO_PIN_8 | GPIO_PIN_9 | GPIO_PIN_10 | GPIO_PIN_11, GPIO_PIN_SET); break;
+	  case 1: HAL_GPIO_WritePin(GPIOC, 				GPIO_PIN_9 | GPIO_PIN_10 | GPIO_PIN_11, GPIO_PIN_SET);
+	  	  	  HAL_GPIO_WritePin(GPIOC, GPIO_PIN_8										  , GPIO_PIN_RESET);break;
+	  case 2: HAL_GPIO_WritePin(GPIOC, 							 GPIO_PIN_10 | GPIO_PIN_11, GPIO_PIN_SET);
+	  	  	  HAL_GPIO_WritePin(GPIOC, GPIO_PIN_8 | GPIO_PIN_9							  , GPIO_PIN_RESET);break;
+	  case 3: HAL_GPIO_WritePin(GPIOC, 										   GPIO_PIN_11, GPIO_PIN_SET);
+	  	  	  HAL_GPIO_WritePin(GPIOC, GPIO_PIN_8 | GPIO_PIN_9 | GPIO_PIN_10			  , GPIO_PIN_RESET);break;
+	  case 4:
+	  case 5: HAL_GPIO_WritePin(GPIOC, GPIO_PIN_8 | GPIO_PIN_9 | GPIO_PIN_10 | GPIO_PIN_11, GPIO_PIN_RESET); break;
+	  }
+	#endif
+	  HAL_Delay(400);
+#elif QUESTION == 5
+		for (int i = 0; i < 4; i++) {
+			char tmp[10];
+			tmp[0] = '0' + i;
+			HAL_UART_Transmit(&huart3, (unsigned char*)"ADC1_CH1", 8, 100);
+			HAL_UART_Transmit(&huart3, (unsigned char*)tmp, 1, 100);
+			HAL_UART_Transmit(&huart3, (unsigned char*)" ", 1, 100);
+			displayHEX(adc_val[i]);
+			HAL_UART_Transmit(&huart3, (unsigned char*)" Vin = ", 7, 100);
+			int a = sprintf(tmp, "%.2f", adc_val[i] * 3.33 / 0x1000);
+			HAL_UART_Transmit(&huart3, (unsigned char*)tmp, a, 100);
+			HAL_UART_Transmit(&huart3, (unsigned char*)"V\r\n", 3, 100);
+		}
+		HAL_Delay(500);
+		HAL_UART_Transmit(&huart3, (unsigned char*)"\r\n\r\n", 4, 100);
+#endif
+    /* USER CODE END WHILE */
+
+    /* USER CODE BEGIN 3 */
+  }
+  /* USER CODE END 3 */
+}
+
+/**
+  * @brief System Clock Configuration
+  * @retval None
+  */
+void SystemClock_Config(void)
+{
+  RCC_OscInitTypeDef RCC_OscInitStruct = {0};
+  RCC_ClkInitTypeDef RCC_ClkInitStruct = {0};
+  RCC_PeriphCLKInitTypeDef PeriphClkInitStruct = {0};
+
+  /** Configure the main internal regulator output voltage
+  */
+  __HAL_RCC_PWR_CLK_ENABLE();
+  __HAL_PWR_VOLTAGESCALING_CONFIG(PWR_REGULATOR_VOLTAGE_SCALE3);
+  /** Initializes the RCC Oscillators according to the specified parameters
+  * in the RCC_OscInitTypeDef structure.
+  */
+  RCC_OscInitStruct.OscillatorType = RCC_OSCILLATORTYPE_HSI;
+  RCC_OscInitStruct.HSIState = RCC_HSI_ON;
+  RCC_OscInitStruct.HSICalibrationValue = RCC_HSICALIBRATION_DEFAULT;
+  RCC_OscInitStruct.PLL.PLLState = RCC_PLL_ON;
+  RCC_OscInitStruct.PLL.PLLSource = RCC_PLLSOURCE_HSI;
+  RCC_OscInitStruct.PLL.PLLM = 8;
+  RCC_OscInitStruct.PLL.PLLN = 120;
+  RCC_OscInitStruct.PLL.PLLP = RCC_PLLP_DIV6;
+  RCC_OscInitStruct.PLL.PLLQ = 2;
+  if (HAL_RCC_OscConfig(&RCC_OscInitStruct) != HAL_OK)
+  {
+    Error_Handler();
+  }
+  /** Initializes the CPU, AHB and APB buses clocks
+  */
+  RCC_ClkInitStruct.ClockType = RCC_CLOCKTYPE_HCLK|RCC_CLOCKTYPE_SYSCLK
+                              |RCC_CLOCKTYPE_PCLK1|RCC_CLOCKTYPE_PCLK2;
+  RCC_ClkInitStruct.SYSCLKSource = RCC_SYSCLKSOURCE_PLLCLK;
+  RCC_ClkInitStruct.AHBCLKDivider = RCC_SYSCLK_DIV4;
+  RCC_ClkInitStruct.APB1CLKDivider = RCC_HCLK_DIV4;
+  RCC_ClkInitStruct.APB2CLKDivider = RCC_HCLK_DIV2;
+
+  if (HAL_RCC_ClockConfig(&RCC_ClkInitStruct, FLASH_LATENCY_0) != HAL_OK)
+  {
+    Error_Handler();
+  }
+  PeriphClkInitStruct.PeriphClockSelection = RCC_PERIPHCLK_USART3;
+  PeriphClkInitStruct.Usart3ClockSelection = RCC_USART3CLKSOURCE_PCLK1;
+  if (HAL_RCCEx_PeriphCLKConfig(&PeriphClkInitStruct) != HAL_OK)
+  {
+    Error_Handler();
+  }
+}
+
+/* USER CODE BEGIN 4 */
+
+/* USER CODE END 4 */
+
+/**
+  * @brief  This function is executed in case of error occurrence.
+  * @retval None
+  */
+void Error_Handler(void)
+{
+  /* USER CODE BEGIN Error_Handler_Debug */
+  /* User can add his own implementation to report the HAL error return state */
+
+  /* USER CODE END Error_Handler_Debug */
+}
+
+#ifdef  USE_FULL_ASSERT
+/**
+  * @brief  Reports the name of the source file and the source line number
+  *         where the assert_param error has occurred.
+  * @param  file: pointer to the source file name
+  * @param  line: assert_param error line source number
+  * @retval None
+  */
+void assert_failed(uint8_t *file, uint32_t line)
+{
+  /* USER CODE BEGIN 6 */
+  /* User can add his own implementation to report the file name and line number,
+     tex: printf("Wrong parameters value: file %s on line %d\r\n", file, line) */
+  /* USER CODE END 6 */
+}
+#endif /* USE_FULL_ASSERT */
+
+/************************ (C) COPYRIGHT STMicroelectronics *****END OF FILE****/
