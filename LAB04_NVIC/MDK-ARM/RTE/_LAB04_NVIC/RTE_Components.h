2649ba7ed7fc05d0d79991b84e8fcda1b72bee2a
Phumphathai Chansriwong <ouoam2555@gmail.com>
2020-11-22 23:05:01 +0700

    Update to CubeMX 6.1.0

--- LAB04_NVIC/MDK-ARM/RTE/_LAB04_NVIC/RTE_Components.h
+++ LAB04_NVIC/MDK-ARM/RTE/_LAB04_NVIC/RTE_Components.h
@@ -3,8 +3,8 @@
  * Auto generated Run-Time-Environment Configuration File
  *      *** Do not modify ! ***
  *
- * Project: 'Lab01_LED' 
- * Target:  'Lab01_LED' 
+ * Project: 'LAB04_NVIC' 
+ * Target:  'LAB04_NVIC' 
  */
 
 #ifndef RTE_COMPONENTS_H
